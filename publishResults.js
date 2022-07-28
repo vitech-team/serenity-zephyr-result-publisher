917fc0c3a149e1c749669ac312e60bc5384ca633
Anton Chudaev <anton.chudaev@vitechteam.com>
2022-07-28 12:12:35 +0300

    Update publishResults.js

--- publishResults.js
+++ publishResults.js
@@ -139,9 +139,9 @@ class PublishResults {
                 let stepResult = []
                 let testCaseKey = this.zephyr.getTestCaseIdByTitle(testCaseName, folderId)
                 this.zephyr.addTestCaseIssueLink(testCaseKey, issueId)
-                let testSteps = json.suites[0].suites[0].specs[0];
+                let testSteps = json.suites[0].suites[0].specs[0].tests[0].results[0].steps[0];
                 let testCaseResult = this.statusPlaywright[json.suites[0].suites[0].specs[0].tests.status]
-                testSteps.tests[0].results[0].steps[0].forEach(step => {
+                testSteps.forEach(step => {
                     steps.push(this.addStep(step.title))
                     stepResult.push(testCaseResult)
                 });
