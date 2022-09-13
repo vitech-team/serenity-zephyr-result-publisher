fd440e8bd69acf88284b86c6bae133e434d00da1
Anton Chudaev <anton.chudaev@vitechteam.com>
2022-09-13 17:29:39 +0300

    Update publishResults.js

--- publishResults.js
+++ publishResults.js
@@ -141,22 +141,26 @@ class PublishResults {
                 let folderName = json.suites[testSuiteSequence].title.split('/')[0];
                 let folderId = this.zephyr.getFolderIdByTitle(folderName);
                 let suiteName = json.suites[testSuiteSequence].specs[0].title;
-                for (let testCaseSequence = 0; testCaseSequence < json.suites[testSuiteSequence].specs[0].tests[0].results[0].steps.length; testCaseSequence++) {
+                
                     let testCaseName = suiteName;
                     let steps = []
                     let stepResult = []            
                     let testCaseKey = this.zephyr.getTestCaseIdByTitle(testCaseName, folderId)
                     this.zephyr.addTestCaseIssueLink(testCaseKey, issueId)
+                
+                
+                for (let testCaseSequence = 0; testCaseSequence < json.suites[testSuiteSequence].specs[0].tests[0].results[0].steps.length; testCaseSequence++) {
+
                     let testSteps = json.suites[testSuiteSequence].specs[0].tests[0].results[0].steps;
                     let testCaseResult = this.statusPlaywright[json.suites[testSuiteSequence].specs[0].tests[0].results[0].status]
                     testSteps.forEach(step => {
                         step.push(this.addStep(step.title))
                         stepResult.push(this.addStepResultPW(testCaseResult,suiteName+"-"+step.title))
                     });
-                
-                this.zephyr.addStepsToTestCase(testCaseKey, steps)
                 this.zephyr.publishResults(cycleKey, testCaseKey, testCaseResult, stepResult)
                 }
+                this.zephyr.addStepsToTestCase(testCaseKey, steps)
+                
                 
             }
 
