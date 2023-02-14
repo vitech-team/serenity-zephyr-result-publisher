3f0608f591d56825a18b4c8abf8cef643bb96b11
mytro.iasynskyi <dmytro.iasynskyi@vitechteam.com>
2023-02-14 11:54:10 +0200

    improvement

--- publishResults.js
+++ publishResults.js
@@ -120,46 +120,16 @@ class PublishResults {
     //
     // }
 
-    async processResults() {
-        let cycleKey = await this.zephyr.addTestRunCycle();
-        let jsonFiles = this.getListOfFiles();
-        for (const fileNameSequence of jsonFiles) {
-            let json = this.readContent(jsonFiles[fileNameSequence]);
-            let issueId = await this.jira.getIssueIdByKey(json.coreIssues);
-            let folderName = json.featureTag.name.split('/')[0];
-            let folderId = await this.zephyr.getFolderIdByTitle(folderName);
-            let suiteName = json.featureTag.name.split('/')[1];
-            for (const testCaseSequence of json.testSteps) {
-                let testCaseName = suiteName;
-                for (let paramSequence = 0; paramSequence < json.dataTable.rows[testCaseSequence].values.length; paramSequence++) {
-                    testCaseName = testCaseName + `: ${json.dataTable.rows[testCaseSequence].values[paramSequence]}`;
-                }
-                let steps = [];
-                let stepResult = [];
-                let testCaseKey = await this.zephyr.getTestCaseIdByTitle(testCaseName, folderId);
-                await this.zephyr.addTestCaseIssueLink(testCaseKey, issueId);
-                let testSteps = json.testSteps[testCaseSequence].children;
-                let testCaseResult = this.status[json.testSteps[testCaseSequence].result];
-                testSteps.forEach(step => {
-                    steps.push(this.addStep(step.description));
-                    stepResult.push(this.addStepResult(step));
-                });
-                await this.zephyr.addStepsToTestCase(testCaseKey, steps);
-                await this.zephyr.publishResults(cycleKey, testCaseKey, testCaseResult, stepResult);
-            }
-        }
-    }
-
     // async processResults() {
     //     let cycleKey = await this.zephyr.addTestRunCycle();
     //     let jsonFiles = this.getListOfFiles();
-    //     let processFiles = jsonFiles.map(async (_, fileNameSequence) => {
+    //     for (const fileNameSequence of jsonFiles) {
     //         let json = this.readContent(jsonFiles[fileNameSequence]);
     //         let issueId = await this.jira.getIssueIdByKey(json.coreIssues);
     //         let folderName = json.featureTag.name.split('/')[0];
     //         let folderId = await this.zephyr.getFolderIdByTitle(folderName);
     //         let suiteName = json.featureTag.name.split('/')[1];
-    //         let processTestCases = json.testSteps.map(async (_, testCaseSequence) => {
+    //         for (const testCaseSequence of json.testSteps) {
     //             let testCaseName = suiteName;
     //             for (let paramSequence = 0; paramSequence < json.dataTable.rows[testCaseSequence].values.length; paramSequence++) {
     //                 testCaseName = testCaseName + `: ${json.dataTable.rows[testCaseSequence].values[paramSequence]}`;
@@ -170,18 +140,48 @@ class PublishResults {
     //             await this.zephyr.addTestCaseIssueLink(testCaseKey, issueId);
     //             let testSteps = json.testSteps[testCaseSequence].children;
     //             let testCaseResult = this.status[json.testSteps[testCaseSequence].result];
-    //             let processStepPromises = testSteps.map(async (step) => {
+    //             testSteps.forEach(step => {
     //                 steps.push(this.addStep(step.description));
     //                 stepResult.push(this.addStepResult(step));
     //             });
-    //             await Promise.all(processStepPromises);
     //             await this.zephyr.addStepsToTestCase(testCaseKey, steps);
     //             await this.zephyr.publishResults(cycleKey, testCaseKey, testCaseResult, stepResult);
-    //         });
-    //         await Promise.all(processTestCases);
-    //     });
-    //     await Promise.all(processFiles);
+    //         }
+    //     }
     // }
+
+    async processResults() {
+        let cycleKey = await this.zephyr.addTestRunCycle();
+        let jsonFiles = this.getListOfFiles();
+        let processFiles = jsonFiles.map(async (_, fileNameSequence) => {
+            let json = this.readContent(jsonFiles[fileNameSequence]);
+            let issueId = await this.jira.getIssueIdByKey(json.coreIssues);
+            let folderName = json.featureTag.name.split('/')[0];
+            let folderId = await this.zephyr.getFolderIdByTitle(folderName);
+            let suiteName = json.featureTag.name.split('/')[1];
+            let processTestCases = json.testSteps.map(async (_, testCaseSequence) => {
+                let testCaseName = suiteName;
+                for (let paramSequence = 0; paramSequence < json.dataTable.rows[testCaseSequence].values.length; paramSequence++) {
+                    testCaseName = testCaseName + `: ${json.dataTable.rows[testCaseSequence].values[paramSequence]}`;
+                }
+                let steps = [];
+                let stepResult = [];
+                let testCaseKey = await this.zephyr.getTestCaseIdByTitle(testCaseName, folderId);
+                await this.zephyr.addTestCaseIssueLink(testCaseKey, issueId);
+                let testSteps = json.testSteps[testCaseSequence].children;
+                let testCaseResult = this.status[json.testSteps[testCaseSequence].result];
+                let processStepPromises = testSteps.map(async (step) => {
+                    steps.push(this.addStep(step.description));
+                    stepResult.push(this.addStepResult(step));
+                });
+                await Promise.all(processStepPromises);
+                await this.zephyr.addStepsToTestCase(testCaseKey, steps);
+                await this.zephyr.publishResults(cycleKey, testCaseKey, testCaseResult, stepResult);
+            });
+            await Promise.all(processTestCases);
+        });
+        await Promise.all(processFiles);
+    }
 
 }
 
