c0962bc7034169aead2d544ef6f37334bbdbf1b0
Dmytro Iasynskyi <dmytro.iasynskyi@vitechteam.com>
2023-08-21 23:44:04 +0300

    zephyr_folder_id_error_fix

--- zephyrScaleClient.js
+++ zephyrScaleClient.js
@@ -1,232 +1,140 @@
-const RestClient = require('./restClient.js')
+const RestClient = require('./restClient.js');
 
-
-/**
- * Zephyr Scale basic API wrapper
- */
 class ZephyrScaleClient extends RestClient {
-
-    /**
-     * Zephyr Scale constructor
-     *
-     * @param options
-     */
-    constructor(options) {
+    constructor({ domain, apiToken, projectKey }) {
         super();
-        this._validate(options, 'domain');
-        this._validate(options, 'apiToken');
-        this._validate(options, 'projectKey');
+        this._validate({ domain });
+        this._validate({ apiToken });
+        this._validate({ projectKey });
 
-        this.options = options;
+        this.options = { domain, apiToken, projectKey };
         this.base = `https://${this.options.domain}/v2/`;
         this.headers = {
             "Authorization": `Bearer ${this.options.apiToken}`,
             "Content-Type": "application/json; charset=utf-8"
-        }
-        this.data = null;
-
+        };
     }
 
-
-    /**
-     * Returns current date in 'Mon Jan 1 2020' format
-     * @return {string}
-     */
     getDateNow() {
-        let now = new Date(Date.now())
-        return now.toDateString()
+        const now = new Date();
+        return now.toDateString();
     }
 
-    /**
-     * Creates testRunCycle in Zephyr Scale via API
-     * @param casprojectKey
-     * @param testRunName
-     * @return testRunId of created testRun
-     */
     async addTestRunCycle(projectKey = this.options.projectKey, testRunName = `Run: ${process.env.RUN_ID} / Branch: ${process.env.BRANCH_NAME} (${this.getDateNow()})`, folderId = this.options.testCycleFolder) {
-        let requestBody = {
+        const requestBody = {
             "projectKey": projectKey,
             "name": testRunName,
             "folderId": folderId
-        }
-        let response = await this._post(`testcycles`, requestBody)
+        };
+        const response = await this._post(`testcycles`, requestBody);
         return response['key'];
     }
 
-    /**
-     * Creates testCase in Zephyr Scale via API
-     * @param name
-     * @param folderId
-     * @return testCaseId of created testCase
-     */
     async addTestCase(name, folderId) {
-        let requestBody = {
+        const requestBody = {
             "projectKey": this.options.projectKey,
             "name": name,
             "folderId": folderId,
             "statusName": 'Approved',
             "ownerId": this.options.ownerId
-        }
-        let response = await this._post(`testcases`, requestBody)
-        return response['key']
+        };
+        const response = await this._post(`testcases`, requestBody);
+        return response['key'];
     }
 
-    /**
-     * Add steps to testCase in Zephyr Scale via API
-     * @param testCaseId
-     * @param steps
-     */
     async addStepsToTestCase(testCaseId, steps) {
-        let requestBody = {
+        const requestBody = {
             "mode": "OVERWRITE",
             "items": steps
-        }
-        await this._post(`testcases/${testCaseId}/teststeps`, requestBody)
+        };
+        await this._post(`testcases/${testCaseId}/teststeps`, requestBody);
     }
 
-    /**
-     * Creates Folder in Zephyr Scale via API
-     * @param name
-     * @return folderId of created section
-     */
     async addFolderId(name, parentId = this.options.parentId) {
-        let requestBody = {
+        const requestBody = {
             "name": name,
             "parentId": parentId,
             "projectKey": this.options.projectKey,
             "folderType": "TEST_CASE"
-        }
-        let response = await this._post(`folders`, requestBody)
-        return response['id']
+        };
+        const response = await this._post(`folders`, requestBody);
+        return response['id'];
     }
 
     filterJson(json, key, value) {
-        let filtered = json.filter(a => a[key] == value);
-        return filtered
+        return json.filter(item => item[key] == value);
     }
 
-    /**
-     * Gets data from api endpoint and returns data matched to key and value
-     * @param api
-     * @param key
-     * @param value
-     * @return {{}}
-     */
-    getDataDictFromApiByParams(api, key, value) {
-        let data = this._get(api)
-        data = data.values
-        let dict = {};
-        for (let i = 0; i < data.length; i++) {
-            dict[data[i][key]] = data[i][value];
+    async getDataDictFromApiByParams(api, key, value) {
+        const data = (await this._get(api)).values;
+        const dict = {};
+        for (const item of data) {
+            dict[item[key]] = item[value];
         }
-        return dict
+        return dict;
     }
 
-    /**
-     * Gets data and returns data matched to key and value
-     * @param data
-     * @param key
-     * @param value
-     * @return {{}}
-     */
     getDataDictByParams(data, key, value) {
-        let dict = {};
-        for (let i = 0; i < data.length; i++) {
-            dict[data[i][key]] = data[i][value];
+        const dict = {};
+        for (const item of data) {
+            dict[item[key]] = item[value];
         }
-        return dict
+        return dict;
     }
 
-    /**
-     * Gets testCaseId based on title and section
-     * in cases there is no such testCase in section, it will be created
-     * @param title
-     * @param folderId
-     * @return testCaseId
-     */
     async getTestCaseIdByTitle(title, folderId) {
-        let data = await this._get(`testcases?projectKey=${this.options.projectKey}&folderId=${folderId}&maxResults=4000`)
-        data = data.values
-        data = this.getDataDictByParams(data, 'name', 'key')
-        let cases = [];
-        for (let name in data) {
-            if (name === title) {
-                cases.push(data[name])
-            }
-        }
+        const data = (await this._get(`testcases?projectKey=${this.options.projectKey}&folderId=${folderId}&maxResults=4000`)).values;
+        const dataDict = this.getDataDictByParams(data, 'name', 'key');
+        const cases = dataDict[title] ? [dataDict[title]] : [];
         if (cases.length > 1) {
-            throw new Error(`In section ${folderId} were found ${cases.length} cases with the same test case name - ${title}`)
+            throw new Error(`In section ${folderId} were found ${cases.length} cases with the same test case name - ${title}`);
         } else if (cases.length === 0) {
-            return await this.addTestCase(title, folderId)
+            return await this.addTestCase(title, folderId);
         } else {
-            return cases[0]
+            return cases[0];
         }
     }
 
-    /**
-     * Gets folderId based on title
-     * in cases there is no such section, it will be created
-     * @param title
-     * @param folderName
-     * @return folderId
-     */
     async getFolderIdByTitle(folderName, title) {
         if (folderName === undefined) {
-            throw new Error(`TestCase "${title}" does not have suite name, please add it`)
+            throw new Error(`TestCase "${title}" does not have suite name, please add it`);
         }
         if (!this.data) {
-            this.data = await this._get(`folders?projectKey=${this.options.projectKey}&folderType=TEST_CASE&maxResults=200`);
+            this.data = (await this._get(`folders?projectKey=${this.options.projectKey}&folderType=TEST_CASE&maxResults=200`)).values;
         }
-        this.data = this.data.values
-        this.data = this.filterJson(this.data, 'parentId', this.options.parentId)
-        this.data = this.getDataDictByParams(this.data, 'name', 'id')
-        let folders = [];
-        for (let name in this.data) {
-            if (name === folderName) {
-                folders.push(this.data[name])
-            }
-        }
+        this.data = this.filterJson(this.data, 'parentId', this.options.parentId);
+        const dataDict = this.getDataDictByParams(this.data, 'name', 'id');
+        const folders = dataDict[folderName] ? [dataDict[folderName]] : [];
         if (folders.length > 1) {
-            throw new Error(`In project ${this.options.projectKey} were found ${folders.length} folders with the same folder name - ${name}`)
+            throw new Error(`In project ${this.options.projectKey} were found ${folders.length} folders with the same folder name - ${folderName}`);
         } else if (folders.length === 0) {
-            return await this.addFolderId(folderName)
+            return await this.addFolderId(folderName);
         } else {
-            return folders[0]
+            return folders[0];
         }
     }
 
-    /**
-     * Add link between testCase in Zephyr and Jira ticket
-     * @param testCaseId
-     * @param issueId
-     */
     async addTestCaseIssueLink(testCaseKey, issueId) {
         if (issueId) {
-            for (let i in issueId) {
-                let requestBody = {
-                    "issueId": issueId[i]
-                }
-                await this._post(`testcases/${testCaseKey}/links/issues`, requestBody, undefined, true)
+            for (const id of issueId) {
+                const requestBody = {
+                    "issueId": id
+                };
+                await this._post(`testcases/${testCaseKey}/links/issues`, requestBody, undefined, true);
             }
         }
     }
 
-    /**
-     * Publish results into Zephyr Scale via API
-     * @param cases
-     * @param results
-     */
     async publishResults(cycleKey, testCaseKey, testCaseResult, stepResult) {
-        let requestBody = {
+        const requestBody = {
             "projectKey": this.options.projectKey,
             "testCycleKey": cycleKey,
             "testCaseKey": testCaseKey,
             "statusName": testCaseResult,
             "testScriptResults": stepResult
-        }
-        await this._post(`testexecutions`, requestBody, undefined)
+        };
+        await this._post(`testexecutions`, requestBody, undefined);
     }
-
 }
 
 module.exports = ZephyrScaleClient;
